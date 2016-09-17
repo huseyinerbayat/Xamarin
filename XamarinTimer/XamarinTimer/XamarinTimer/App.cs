98808d2a07fd998f5442dac39e093c00eb3a995c
huseynerbayat <huseyn_erbayat@hotmail.com>
2016-09-17 15:41:31 +0300

    Xamarin'de Timer Kullanımı

---
+++ XamarinTimer/XamarinTimer/XamarinTimer/App.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace XamarinTimer
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = new TimerPage
+            {
+                
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ XamarinTimer/XamarinTimer/XamarinTimer/TimerPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XamarinTimer
+{
+    public partial class TimerPage : ContentPage
+    {
+        public TimerPage()
+        {
+            InitializeComponent();
+        }
+        private void timer(double time)
+        {
+            Device.StartTimer(TimeSpan.FromSeconds(1), () => {
+                time -= 1;
+                _lblTime.Text = String.Format("{0}", time);
+                if (time == 0.00)
+                {
+                    DisplayAlert("Süre Doldu", "Geri sayım süresi bitti!", "Ok");
+                    return false;
+                }
+
+                return true;
+            });
+        }
+
+        private void TimerStarterClicked(object sender,EventArgs e)
+        {
+            double time = double.Parse( _EntryTime.Text);
+            timer(time);
+        }
+    }
+}
