dc9facf540f50f2f4da135a3f3b23ba448012a3b
huseynerbayat <huseyn_erbayat@hotmail.com>
2016-12-05 21:25:58 +0300

    XamarimPopup eklendi

---
+++ XamarinPopUp/XamarinPopUp/XamarinPopUp/Views/MyPopUp.cs
@@ -0,0 +1,74 @@
+﻿using Rg.Plugins.Popup.Pages;
+using Xamarin.Forms;
+
+namespace XamarinPopUp.Views
+{
+    class MyPopUp : PopupPage
+    {
+        public MyPopUp()
+        {
+            Label _lblTitle = new Label()
+            {
+                Text = "Xamarin Popup Title",
+                FontSize = 28,
+                TextColor = Color.Red
+            };
+
+            StackLayout _stkTitle = new StackLayout
+            {
+                Children =
+                {
+                    _lblTitle
+                }
+            };
+
+            Label _lblContent = new Label()
+            {
+                Text = "Xamarin Popup Content",
+                FontSize = 24,
+                TextColor = Color.Blue
+            };
+
+            StackLayout _stkContent = new StackLayout
+            {
+                Children =
+                {
+                    _lblContent
+                }
+            };
+
+            Image _img = new Image()
+            {
+                Source= ImageSource.FromResource( "XamarinPopUp.Images.xamarin.jpg"),
+                HeightRequest = 50,
+                WidthRequest = 50
+            };
+
+            StackLayout _stk_Img = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Center,
+                Children =
+                {
+                    _img
+                }
+            };
+            StackLayout _stk = new StackLayout
+            {
+                BackgroundColor = Color.White,
+                Orientation = StackOrientation.Vertical,
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Center,
+                Padding = new Thickness(10),
+                Children =
+                {
+                    _stkTitle,
+                    _stkContent,
+                    _stk_Img
+                }
+            };
+
+            Content = _stk;
+        }
+    }
+}
---
+++ XamarinPopUp/XamarinPopUp/XamarinPopUp/Views/ShowPopupPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Rg.Plugins.Popup.Extensions;
+using System;
+
+using Xamarin.Forms;
+
+namespace XamarinPopUp.Views
+{
+    public partial class ShowPopupPage : ContentPage
+    {
+        public ShowPopupPage()
+        {
+            InitializeComponent();
+
+            BackgroundColor = Color.White;
+
+            Button _btnShowPopUp = new Button()
+            {
+                Text = "Popup Göster",
+                TextColor = Color.Aqua,
+                BackgroundColor = Color.Blue
+            };
+            _btnShowPopUp.Clicked += _btnShowPopUp_Clicked;
+            StackLayout _stk = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Center,
+                Children =
+                {
+                    _btnShowPopUp
+                }
+            };
+            this.Content = _stk;
+        }
+
+        private void _btnShowPopUp_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushPopupAsync(new MyPopUp());
+        }
+    }
+}
+    
